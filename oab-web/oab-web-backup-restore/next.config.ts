3b0e79d3d7713e5d388121e8bc9bb306f423215d
Deji Akiboye <medicinaltruths@gmail.com>
2025-10-11 15:40:01 +0100

    Delete oab-web/oab-web-backup-restore directory
    delete backup restore directory

--- oab-web/oab-web-backup-restore/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // Disable the Next.js Dev Tools overlay in dev (Next 15+)
-  devIndicators: false,
-};
-
-export default nextConfig;
